d9c98a3170af1a9b12b6694a36de1857fb4e74b4
miro <arek47@gmail.com>
2018-12-16 17:05:35 +0100

    improve layout in word-import.component.html

--- src/main/webapp/app/entities/dictionary/dictionary.service.ts
+++ src/main/webapp/app/entities/dictionary/dictionary.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { SERVER_API_URL } from 'app/app.constants';
--- src/main/webapp/app/layouts/import/word-import/word-import.component.ts
+++ src/main/webapp/app/layouts/import/word-import/word-import.component.ts
@@ -4,13 +4,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Dictionary, IDictionary } from 'app/shared/model/dictionary.model';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { JhiAlertService } from 'ng-jhipster';
-import { Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
 
 @Component({
     selector: 'jhi-word-import',
     templateUrl: './word-import.component.html',
-    styles: []
+    styleUrls: ['word-import.css']
 })
 export class WordImportComponent implements OnInit {
     readonly importType: string;
@@ -51,24 +51,32 @@ export class WordImportComponent implements OnInit {
 
     onUpload() {
         if (this.dictionary.title) {
-            this.dictionaryService
-                .create(this.dictionary)
-                .pipe(
-                    map((res: HttpResponse<IDictionary>) => res.body.id),
-                    switchMap(id => (id ? this.dictionaryService.uploadFile(this.uploadedFile, id, this.importType) : of(null)))
-                )
-                .subscribe(
-                    (res: HttpResponse<IDictionary>) => this.goToDictionary(res.body.id),
-                    (res: HttpErrorResponse) => this.onError(res.message)
-                );
+            this.createAndUpdateDictionary();
         } else {
-            this.dictionaryService
-                .uploadFile(this.uploadedFile, this.selectedDictionary.id, this.importType)
-                .subscribe(
-                    (res: HttpResponse<IDictionary>) => this.goToDictionary(res.body.id),
-                    (res: HttpErrorResponse) => this.onError(res.message)
-                );
+            this.updateDictionary();
         }
+    }
+
+    private updateDictionary() {
+        this.dictionaryService
+            .uploadFile(this.uploadedFile, this.selectedDictionary.id, this.importType)
+            .subscribe(
+                (res: HttpResponse<IDictionary>) => this.goToDictionary(res.body.id),
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
+    }
+
+    private createAndUpdateDictionary() {
+        this.dictionaryService
+            .create(this.dictionary)
+            .pipe(
+                map((res: HttpResponse<IDictionary>) => res.body.id),
+                switchMap(id => (id ? this.dictionaryService.uploadFile(this.uploadedFile, id, this.importType) : of(null)))
+            )
+            .subscribe(
+                (res: HttpResponse<IDictionary>) => this.goToDictionary(res.body.id),
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
     }
 
     private onError(errorMessage: string) {
